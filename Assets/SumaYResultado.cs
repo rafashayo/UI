5d5319afecc1ea34e4d9ba3d85e5bfa4aa5105c7
rafashayo <49433616@est.ort.edu.ar>
2025-05-13 09:00:38 -0300

    Script

---
+++ Assets/SumaYResultado.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SumaYResultado : MonoBehaviour
+{
+    public TMP_InputField InputNum1;
+    public TMP_InputField InputNum2;
+    public TextMeshProUGUI TxtResultado;
+    int num1;
+    int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TxtResultado.text = "";
+    }
+
+    public void SumarYMostrar()
+    {
+        num1 = int.Parse(InputNum1.text);
+        num2 = int.Parse(InputNum2.text);
+        TxtResultado.text = (num1 + num2).ToString();
+    }   
+}
